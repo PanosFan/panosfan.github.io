a951639917d5c41b0388ca2c7762a197665c509e
Panagiotis <panfanartzis@gmail.com>
2022-07-18 17:00:06 +0300

    skills section (in progress)

--- src/Components/Layout/Skills/Skills.jsx
+++ src/Components/Layout/Skills/Skills.jsx
@@ -3,7 +3,57 @@ import "./Skills.scss";
 const Skills = () => {
   return (
     <section data-aos="fade-up" id="skills">
-      <h4>Skills</h4>
+      <h4>My experience</h4>
+      <div className="cards">
+        <div className="frontEnd card">
+          <h5>Frontend Development</h5>
+          <div className="content">
+            <ul>
+              <li>
+                <div className="skill">HTML</div>
+                <div className="experience">Intermediate</div>
+              </li>
+              <li>
+                <div className="skill">CSS</div>
+                <div className="experience">Intermediate</div>
+              </li>
+              <li>
+                <div className="skill">JavaScript</div>
+                <div className="experience">Intermediate</div>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <div className="skill">Bootstrap</div>
+                <div className="experience">Experienced</div>
+              </li>
+              <li>
+                <div className="skill">React</div>
+                <div className="experience">Experienced</div>
+              </li>
+              <div className="skill">TailWind</div>
+              <div>Basic</div>
+            </ul>
+          </div>
+        </div>
+        <div className="backEnd card">
+          <h5>Backend Development</h5>
+          <ul>
+            <li>
+              <div className="skill">MySQL</div>
+              <div className="experience">Experienced</div>
+            </li>
+            <li>
+              <div className="skill">PHP</div>
+              <div className="experience">Intermediate</div>
+            </li>
+            <li>
+              <div className="skill">Laravel</div>
+              <div className="experience">Experienced</div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
